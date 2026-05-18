f44b6f0358fc507f7a8d3aa7792345cfa8c0a2f5
Jatin Siradhana <220714886+siradhanajatin@users.noreply.github.com>
2026-05-18 16:40:13 +0530

    Fill the surface with rgb color and add a delay to screen termination - window can now open for 3 seconds using the SDL_Delay command - the window has rgb color filled using a nested for loop

--- iv.c
+++ iv.c
@@ -4,8 +4,30 @@
 
 int main() {
 
-    printf("Helow, The World!");
-    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", 900, 600, 0);
+    const int width = 900;
+    const int height = 600;
+    
+    SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", width, height, 0);
+    SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
+
+
+    Uint8 r,g,b;
+    r=0x1A;
+    g=b=0x00;
+    Uint32 color = SDL_MapSurfaceRGB(psurface, r, g, b);  
+    
+    SDL_Rect pixel = (SDL_Rect){0,0,1,1};
+    for (int x=0; x<width; x++) {
+        for (int y=0; y<height; y++) {
+            pixel.x=x;
+            pixel.y=y;
+            SDL_FillSurfaceRect(psurface, &pixel, color);
+        }
+    }
+    SDL_UpdateWindowSurface(pwindow);
+
+    SDL_Delay(3000);
+
 
     return 0;
 }
