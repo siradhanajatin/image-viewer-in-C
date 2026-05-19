3b071b23b363857071cfa18a7b4cac485c1229ec
Jatin Siradhana <220714886+siradhanajatin@users.noreply.github.com>
2026-05-19 15:26:37 +0530

    Program now works fully functionally and the changes made are specified below: - Change the `fgets()` and `sscanf()` approach with a user-defined function and `fopen()`. Now the program won't read spaces or comment lines - Interchange the variables of the nested for loop as the file has opposite structure of rgb values - Replace `getchar()` with `fgetc(in)`. Now instead of reading the keyboard input, the program reads the next byte from the file pointed by `in` - `next_token()` function help skip all the whitespace or comments

--- iv.c
+++ iv.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <SDL3/SDL.h>
+
+static int next_token(FILE *in, char *buf, size_t bufsize) {
+    int c;
+    while ((c = fgetc(in)) != EOF) {
+        if (c == '#') {
+            while ((c = fgetc(in)) != EOF && c != '\n');
+            continue;
+        }
+        if (!isspace(c)) {
+            size_t i = 0;
+            buf[i++] = (char)c;
+            while (i + 1 < bufsize) {
+                c = fgetc(in);
+                if (c == EOF || isspace(c)) break;
+                buf[i++] = (char)c;
+            }
+            buf[i] = '\0';
+            return 1;
+        }
+    }
+    return 0;
+}
 
 int main() {
     FILE *in = fopen("image.ppm", "rb");
@@ -9,58 +32,41 @@ int main() {
         return 1;
     }
 
-    char header[1000];
-    char comment[1000];
-    char dimensions[1000];
+    char token[128];
+    next_token(in, token, sizeof(token));
 
-    if (!fgets(header, sizeof(header), in)) {
-        fprintf(stderr, "Failed to read PPM header\n");
-        fclose(in);
-        return 1;
-    }
+    next_token(in, token, sizeof(token));
+    int width = atoi(token);
 
-    if (!fgets(comment, sizeof(comment), in)) {
-        fprintf(stderr, "Failed to read PPM comment line\n");
-        fclose(in);
-        return 1;
-    }
+    next_token(in, token, sizeof(token));
+    int height = atoi(token);
 
-    if (!fgets(dimensions, sizeof(dimensions), in)) {
-        fprintf(stderr, "Failed to read image dimensions\n");
-        fclose(in);
-        return 1;
-    }
-
-    int width = -1;
-    int height = -1;
-    if (sscanf(dimensions, "%d %d", &width, &height) != 2) {
-        fprintf(stderr, "Invalid dimensions line: %s", dimensions);
-        fclose(in);
-        return 1;
-    }
-
-    printf("width=%d, height=%d\n", width, height);
-    fclose(in);
+    next_token(in, token, sizeof(token));
+    int maxval = atoi(token);
 
     SDL_Window *pwindow = SDL_CreateWindow("Image Viewer", width, height, 0);
     SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-    Uint8 r,g,b;
-    r=0x1A;
-    g=b=0x00;
-    Uint32 color = SDL_MapSurfaceRGB(psurface, r, g, b);  
-    
     SDL_Rect pixel = (SDL_Rect){0,0,1,1};
-    for (int x=0; x<width; x++) {
-        for (int y=0; y<height; y++) {
-            pixel.x=x;
-            pixel.y=y;
+    Uint32 color = 0;
+    for (int y=0; y<height; y++) {
+        for (int x=0; x<width; x++) {
+            int ri = fgetc(in);
+            int gi = fgetc(in);
+            int bi = fgetc(in);
+            Uint8 r = (Uint8)ri;
+            Uint8 g = (Uint8)gi;
+            Uint8 b = (Uint8)bi;
+            color = SDL_MapSurfaceRGB(psurface, r, g, b);
+            pixel.x = x;
+            pixel.y = y;
             SDL_FillSurfaceRect(psurface, &pixel, color);
         }
     }
+
     SDL_UpdateWindowSurface(pwindow);
 
-    SDL_Delay(3000);
+    SDL_Delay(5000);
 
     SDL_DestroyWindow(pwindow);
     SDL_Quit();
